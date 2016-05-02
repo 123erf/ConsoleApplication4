069a58f4e194fb3c59a6a0fc53b3fbe6dbedb276
123erf <erfan_yahya@hotmail.com>
2016-05-01 22:17:44 -0400

    Added moving functionality to object of interest.

--- ConsoleApplication4/common.h
+++ ConsoleApplication4/common.h
@@ -19,10 +19,48 @@ void printd(char* msg) {
 	}
 }
 
+void setPoint(int (&array)[HEIGHT][WIDTH], int x, int y) {
+	if (y > HEIGHT || x > WIDTH || x < 0 || y < 0) {
+		cerr << "ERROR: OUT OF RANGE: " << x << "," << y;
+	}
+	else {
+		array[y][x] = 1;
+
+	}
+}
+
+void moveUp(int(&array)[HEIGHT][WIDTH], int (&x), int(&y)) {
+	array[y][x] = 0;
+	y = y - 1;
+	if(y<0) y = HEIGHT - 1;
+	array[y][x] = 1;
+}
+
+void moveDown(int(&array)[HEIGHT][WIDTH], int(&x), int(&y)) {
+	array[y][x] = 0;
+	y = y + 1;
+	if (y> HEIGHT - 1) y = 0;
+	array[y][x] = 1;
+}
+
+void moveLeft(int(&array)[HEIGHT][WIDTH], int(&x), int(&y)) {
+	array[y][x] = 0;
+	x = x - 1;
+	if (x  < 0) x = WIDTH - 1;
+	array[y][x] = 1;
+}
+
+void moveRight(int(&array)[HEIGHT][WIDTH], int(&x), int(&y)) {
+	array[y][x] = 0;
+	x = x + 1;
+	if (x > WIDTH - 1) x = 0;
+	array[y][x] = 1;
+}
+
 void displayGrid(int array[HEIGHT][WIDTH]) {
 	for (int i = 0; i < HEIGHT; i++) {
 		for (int j = 0; j < WIDTH; j++) {
-			cout << array[i][j];
+			cout << array[i][j] << " ";
 		}
 		cout << "\n";
 	}
--- ConsoleApplication4/main.cpp
+++ ConsoleApplication4/main.cpp
@@ -11,11 +11,38 @@ int main(void) {
 	cout << "Height:" << HEIGHT << " Width:" << WIDTH << "\n";
 	int array[HEIGHT][WIDTH] = { 0 };
 	displayGrid(array);
+
+	int x_pos, y_pos;
+
+	x_pos = WIDTH / 2;
+	y_pos = HEIGHT / 2;
+	setPoint(array, x_pos,y_pos);
+	//array[y_pos][x_pos] = 1;
+	cout << "TEST x:" << x_pos << " y:" << y_pos << "\n";
+	displayGrid(array);
+
 	char userInput = '0';
 	while (true && userInput != 'q'){
 		cout << "Enter Input\n";
 		cin >> userInput;
 		cout << "You Entered " << userInput << "\n";
+		switch (userInput) {
+			case 'w':
+				moveUp(array, x_pos, y_pos);
+				break;
+			case 'a':
+				moveLeft(array, x_pos, y_pos);
+				break;
+			case 's':
+				moveDown(array, x_pos, y_pos);
+				break;
+			case 'd':
+				moveRight(array, x_pos, y_pos);
+				break;
+			default:
+				break;
+		}
+		displayGrid(array);
 	}
 
 
