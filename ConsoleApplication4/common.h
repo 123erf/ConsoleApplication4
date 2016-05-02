14eb78a6aabae4b587491eb47773e122bc5be9df
123erf <erfan_yahya@hotmail.com>
2016-05-01 21:42:20 -0400

    Created Grid Object and displayGrid function. Created userinput feature.

--- ConsoleApplication4/common.h
+++ ConsoleApplication4/common.h
@@ -8,19 +8,24 @@
 
 #define DEBUG_M true
 
+
+#define HEIGHT 10
+#define WIDTH 20
 using namespace std;
 
 void printd(char* msg) {
-	if (DEBUG_M == true) {
+	if (DEBUG_M == true && msg!=NULL) {
 		cout << msg;
 	}
-
-
 }
 
-
-
-
-
+void displayGrid(int array[HEIGHT][WIDTH]) {
+	for (int i = 0; i < HEIGHT; i++) {
+		for (int j = 0; j < WIDTH; j++) {
+			cout << array[i][j];
+		}
+		cout << "\n";
+	}
+}
 
 #endif COMMON_H
--- ConsoleApplication4/main.cpp
+++ ConsoleApplication4/main.cpp
@@ -6,5 +6,17 @@ using namespace std;
 
 int main(void) {
 	printd("Hello World\n");
+	
+	//Create GRID
+	cout << "Height:" << HEIGHT << " Width:" << WIDTH << "\n";
+	int array[HEIGHT][WIDTH] = { 0 };
+	displayGrid(array);
+	char userInput = '0';
+	while (true && userInput != 'q'){
+		cout << "Enter Input\n";
+		cin >> userInput;
+		cout << "You Entered " << userInput << "\n";
+	}
+
 
 }
