7ff16def436077e2472a2a576c9799d1cffafa51
123erf <erfan_yahya@hotmail.com>
2016-05-01 23:49:52 -0400

    More updates, added enemy player and score functionality.

--- ConsoleApplication4/common.h
+++ ConsoleApplication4/common.h
@@ -7,10 +7,15 @@
 #include <string>
 
 #define DEBUG_M true
+#define PLAYER_ID 1
+#define ENEMY_ID 2
 
 
-#define HEIGHT 10
-#define WIDTH 20
+#define ENEMY_KILLED 100
+
+
+#define HEIGHT 20
+#define WIDTH 40
 using namespace std;
 
 void printd(char* msg) {
@@ -19,56 +24,86 @@ void printd(char* msg) {
 	}
 }
 
-void setPoint(int (&array)[HEIGHT][WIDTH], int x, int y) {
+void setPoint(int (&array)[HEIGHT][WIDTH], int x, int y, int ID) {
 	if (y > HEIGHT || x > WIDTH || x < 0 || y < 0) {
 		cerr << "ERROR: OUT OF RANGE: " << x << "," << y;
 	}
 	else {
-		array[y][x] = 1;
-
+		array[y][x] = ID;
 	}
 }
 
-void moveUp(int(&array)[HEIGHT][WIDTH], int (&x), int(&y)) {
+int moveUp(int(&array)[HEIGHT][WIDTH], int (&x), int(&y)) {
+	int status = 0;
 	array[y][x] = 0;
 	y = y - 1;
 	if(y<0) y = HEIGHT - 1;
+	if (array[y][x] == ENEMY_ID) {
+		status = ENEMY_KILLED;
+	}
 	array[y][x] = 1;
+
+	return status;
 }
 
-void moveDown(int(&array)[HEIGHT][WIDTH], int(&x), int(&y)) {
+int moveDown(int(&array)[HEIGHT][WIDTH], int(&x), int(&y)) {
+	int status = 0;
 	array[y][x] = 0;
 	y = y + 1;
 	if (y> HEIGHT - 1) y = 0;
+	if (array[y][x] == ENEMY_ID) {
+		status = ENEMY_KILLED;
+	}
 	array[y][x] = 1;
+
+	return status;
 }
 
-void moveLeft(int(&array)[HEIGHT][WIDTH], int(&x), int(&y)) {
+int moveLeft(int(&array)[HEIGHT][WIDTH], int(&x), int(&y)) {
+	int status = 0;
 	array[y][x] = 0;
 	x = x - 1;
 	if (x  < 0) x = WIDTH - 1;
+	if (array[y][x] == ENEMY_ID) {
+		status = ENEMY_KILLED;
+	}
 	array[y][x] = 1;
+
+	return status;
 }
 
-void moveRight(int(&array)[HEIGHT][WIDTH], int(&x), int(&y)) {
+int moveRight(int(&array)[HEIGHT][WIDTH], int(&x), int(&y)) {
+	int status = 0;
 	array[y][x] = 0;
 	x = x + 1;
 	if (x > WIDTH - 1) x = 0;
+	if (array[y][x] == ENEMY_ID) {
+		status = ENEMY_KILLED;
+	}
 	array[y][x] = 1;
+
+
+	return status;
 }
 
 void displayGrid(int array[HEIGHT][WIDTH]) {
+	cout << "==================================================================================\n";
 	for (int i = 0; i < HEIGHT; i++) {
 		for (int j = 0; j < WIDTH; j++) {
+			if (j == 0) cout << "|";
 			if (array[i][j] == 0) {
 				cout << " " << " ";
 			}
-			else {
-				cout << "X" << " ";
+			else if(array[i][j] == PLAYER_ID){
+				cout << "O" << " ";
 			}
+			else if (array[i][j] == ENEMY_ID){
+				cout << "X" << " ";
+			}	
 		}
-		cout << "\n";
+		cout << "|\n";
 	}
+	cout << "==================================================================================\n";
 }
 
 #endif COMMON_H
--- ConsoleApplication4/main.cpp
+++ ConsoleApplication4/main.cpp
@@ -1,27 +1,38 @@
 #include <iostream>
 #include <stdio.h>
 #include <conio.h>
+#include <stdlib.h>
+
+
 #include "common.h"
 using namespace std;
 
 int main(void) {
-	printd("Hello World\n");
-	
+	int x_pos, y_pos;
+	int score = 0;
+	int ex_pos, ey_pos;
+	int array[HEIGHT][WIDTH] = { 0 };
+	int status = 0;
 	//Create GRID
 	cout << "Height:" << HEIGHT << " Width:" << WIDTH << "\n";
-	int array[HEIGHT][WIDTH] = { 0 };
 	displayGrid(array);
+	
+	ex_pos = rand() % WIDTH;
+	ey_pos = rand() % HEIGHT;
 
-	int x_pos, y_pos;
 
 	x_pos = WIDTH / 2;
 	y_pos = HEIGHT / 2;
-	setPoint(array, x_pos,y_pos);
-	//array[y_pos][x_pos] = 1;
+	
+	setPoint(array, x_pos,y_pos, PLAYER_ID);
+	setPoint(array, ex_pos, ey_pos, ENEMY_ID);
+	
 	cout << "TEST x:" << x_pos << " y:" << y_pos << "\n";
+	system("CLS");
 	displayGrid(array);
 
 	char userInput = '0';
+
 	while (true && userInput != 'q'){
 		cout << "Enter Input\n";
 		userInput = _getch();
@@ -29,21 +40,31 @@ int main(void) {
 		cout << "You Entered " << userInput << "\n";
 		switch (userInput) {
 			case 'w':
-				moveUp(array, x_pos, y_pos);
+				status = moveUp(array, x_pos, y_pos);
 				break;
 			case 'a':
-				moveLeft(array, x_pos, y_pos);
+				status = moveLeft(array, x_pos, y_pos);
 				break;
 			case 's':
-				moveDown(array, x_pos, y_pos);
+				status = moveDown(array, x_pos, y_pos);
 				break;
 			case 'd':
-				moveRight(array, x_pos, y_pos);
+				status = moveRight(array, x_pos, y_pos);
 				break;
 			default:
 				break;
 		}
 		displayGrid(array);
+		if (status == ENEMY_KILLED) {
+			cout << "ENEMY KILLED!!\n"; 
+			score += 10;
+			ex_pos = rand() % WIDTH;
+			ey_pos = rand() % HEIGHT;
+			setPoint(array, ex_pos, ey_pos, ENEMY_ID);
+		}
+		cout << "Player Position (" << x_pos << ", " << y_pos << ")\n";
+		cout << "Player Score (" << score << ")\n";
+		//displayInfo(array, x_pos, y_pos);
 	}
 
 
